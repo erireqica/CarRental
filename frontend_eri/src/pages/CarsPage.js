a5d9c94129298dd60197706782fe48a037d5d8dc
Eri <erireqica5@gmail.com>
2025-04-20 18:50:31 +0200

    Tailwind setup

--- frontend_eri/src/pages/CarsPage.js
+++ frontend_eri/src/pages/CarsPage.js
@@ -10,12 +10,13 @@ function CarsPage() {
 
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Available Cars</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Available Cars</h1>
       <ul>
         {cars.map(car => (
           <li key={car.id}>
             {car.name} - {car.brand} - ${car.price_per_day}
           </li>
+
         ))}
       </ul>
     </div>
